421bdd13aa94d45ecb475f11c5c7796b2cc25826
Justin Cook <jtcook19@gmail.com>
2018-01-13 11:35:47 -0700

    Cards shuffle and class assignment

--- js/app.js
+++ js/app.js
@@ -1,28 +1,46 @@
-/*
- * Create a list that holds all of your cards
- */
- const deck = {
-   cards : [
-     anchor, anchor,
-     bicycle, bicycle,
-     bolt, bolt,
-     bomb, bomb
-     cube, cube,
-     diamond, diamond,
-     leaf, leaf,
-     paper-plane-o, paper-plane-o,
-   ],
-   opened : [],
-   matched : [],
-   // Fisher-Yates shuffle found at https://www.frankmitchell.org/2015/01/fisher-yates/
-   shuffle : (array) => {
-     let i = 0, j = 0, temp = null;
+"use strict";
 
-     for (i = array.length - 1; i > 0; i -= 1){
-       j = Math.floor(Math.random() * (i + 1));
-       temp = array[i];
-       array[i] = array[j];
-       array[j] = temp;
-     }
-   },
+// Game deck
+const deck = {
+ cards : [
+   'microchip', 'microchip',
+   'bluetooth', 'bluetooth',
+   'code-fork', 'code-fork',
+   'code', 'code',
+   'database', 'database',
+   'desktop', 'desktop',
+   'server', 'server',
+   'terminal', 'terminal'
+ ],
+ opened : [],
+ matched : [],
+  // Shuffle function from http://stackoverflow.com/a/2450976
+ shuffle: function(array) {
+   var currentIndex = array.length, temporaryValue, randomIndex;
+
+   while (currentIndex !== 0) {
+       randomIndex = Math.floor(Math.random() * currentIndex);
+       currentIndex -= 1;
+       temporaryValue = array[currentIndex];
+       array[currentIndex] = array[randomIndex];
+       array[randomIndex] = temporaryValue;
+   }
+
+   return array;
  }
+}
+
+buildGame();
+
+// Build Game
+function buildGame(){
+  // Shuffle game deck
+  deck.cards = deck.shuffle(deck.cards);
+
+  // array containing all card elements
+  var cards = $(".card");
+  for(var i = 0; i < cards.length; i++){
+    cards[i].className = "card hidden"
+    cards[i].innerHTML = "<i class=\"fa fa-" + deck.cards[i] + "\"></i>"
+  }
+}
